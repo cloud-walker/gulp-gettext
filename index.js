416454146fecbb76dd804438a0aba0002ba63a5c
Luca Barone <luca.b@docsity.com>
2015-03-19 13:19:53 +0100

    first version

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-module.exports = function() {};
+var gp, gutil, through;
+
+through = require('through2');
+
+gutil = require('gulp-util');
+
+gp = require('gettext-parser');
+
+module.exports = function() {
+  var stream;
+  return stream = through.obj(function(file, enc, cb) {
+    var moBuffer, parsedPo;
+    parsedPo = gp.po.parse(file.contents);
+    moBuffer = gp.mo.compile(parsedPo);
+    file.contents = moBuffer;
+    file.path = gutil.replaceExtension(file.path, '.mo');
+    this.push(file);
+    return cb();
+  });
+};
