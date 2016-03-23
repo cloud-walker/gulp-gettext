536b4162b2c63afb102c10f5f3f2d424b1f393f5
cloud-walker <baro.luc@gmail.com>
2016-03-23 14:37:12 +0100

    Fixes commonjs module exports

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import through from 'through2'
 import {po, mo} from 'gettext-parser'
 import gutil from 'gulp-util'
 
-export default function () {
+module.exports = function () {
 	return through.obj(function (file, enc, done) {
 		const parsed = po.parse(file.contents)
 		const buffer = mo.compile(parsed)
